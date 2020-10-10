01beeff323c70613871d2ddf0c4187aecc9447f9
Jaro32768 <jarko04@hotmail.sk>
2020-10-10 13:18:14 +0200

    vypracovanie

--- Open-Lab-05.01/DateTools.cs
+++ Open-Lab-05.01/DateTools.cs
@@ -6,7 +6,21 @@ namespace Open_Lab_05._01
     {
         public string Century(int year)
         {
-            throw new NotImplementedException();
+            int a = year + 99;
+            int b = a / 100;
+            string c = b.ToString();
+            string d;
+            if (c.EndsWith("11") || c.EndsWith("12") || c.EndsWith("13" ))
+                d = c + "th century";
+            else if (c.EndsWith("1"))
+                d = c + "st century";
+            else if (c.EndsWith("2"))
+                d = c + "nd century";
+            else if(c.EndsWith("3"))
+                d = c + "rd century";
+            else
+                d = c + "th century";
+            return d;
         }
     }
 }
